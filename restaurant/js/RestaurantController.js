2aa206194bb42948fb828b494c43e334fef187ab
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-23 15:12:55 -0800

    Moving on from restaurants

--- restaurant/js/RestaurantController.js
+++ restaurant/js/RestaurantController.js
@@ -5,7 +5,7 @@ app.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
       name: $scope.restaurantName,
       cuisine: $scope.restaurantCuisine,
       price: $scope.restaurantPrice,
-      rating: [$scope.restaurantRating]
+      rating: [parseInt($scope.restaurantRating)]
     });
     $scope.restaurantName = null;
     $scope.restaurantCuisine = null;
@@ -16,6 +16,24 @@ app.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
     $scope.restaurants.splice($scope.restaurants.indexOf(item), 1);
   };
   $scope.reRateRestaurant = function(item, newRating) {
-    $scope.restaurants[$scope.restaurants.indexOf(item)].rating.push(newRating);
+    item.rating.push(parseInt(newRating));
+  };
+  $scope.ratingTotal = function(item) {
+    var runningTotal = 0;
+    item.rating.forEach(function(ratingScore){
+      runningTotal += ratingScore;
+    });
+    var avg = Math.round(runningTotal/item.rating.length);
+
+    var starDisplay = "";
+    for(var i=0;i<5;i++) {
+      if(i<avg) {
+        starDisplay = starDisplay + "<i class='fa fa-star'></i>";
+      } else {
+        starDisplay = starDisplay + "<i class='fa fa-star-o'></i>";
+      }
+    }
+    debugger;
+    $("#rating-total").html(starDisplay);
   };
 });
