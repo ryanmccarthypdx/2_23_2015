1d754b731944dbb6237e9e00a5cacc5a3c95cb1a
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-23 12:22:20 -0800

    add array for rating

--- restaurant/js/RestaurantController.js
+++ restaurant/js/RestaurantController.js
@@ -5,14 +5,17 @@ app.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
       name: $scope.restaurantName,
       cuisine: $scope.restaurantCuisine,
       price: $scope.restaurantPrice,
-      rating: $scope.restaurantRating
+      rating: [$scope.restaurantRating]
     });
     $scope.restaurantName = null;
     $scope.restaurantCuisine = null;
     $scope.restaurantPrice = "";
-    $scope.restaurantRating = null;
+    // $scope.restaurantRating = null;
   };
   $scope.deleteRestaurant = function(item) {
     $scope.restaurants.splice($scope.restaurants.indexOf(item), 1);
+  };
+  $scope.reRateRestaurant = function(item, newRating) {
+    $scope.restaurants[$scope.restaurants.indexOf(item)].rating.push(newRating);
   };
 });
